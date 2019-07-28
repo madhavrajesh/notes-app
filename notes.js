722ce6050a2f59f7282a2dc143a23b110e8f18c6
madhavrajesh <53228268+madhavrajesh@users.noreply.github.com>
2019-07-28 23:51:28 +0530

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,73 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const getNotes = () => {
+    return 'Your notes...'
+}
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNote = notes.find((note) => note.title === title)
+    debugger
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
+    } else {
+        console.log(chalk.red.inverse('Note title taken!'))
+    }
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter((note) => note.title !== title)
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
+    }    
+}
+
+const listNotes = () => {
+    notes = loadNotes()
+    console.log(chalk.white.inverse("Your Notes"))
+    notes.forEach((note) => console.log(note.title));
+}
+
+const readNotes = (title) => {
+    const notes = loadNotes()
+    const noteFound = notes.find((note) => note.title === title)
+    if (noteFound){
+    console.log(chalk.inverse(noteFound.title))
+    console.log(noteFound.body)} else {
+        console.log(chalk.red.inverse('No note found!'))
+    }
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNotes: readNotes
+}
